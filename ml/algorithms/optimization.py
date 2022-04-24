Fix regularization and scaling, assignment 1
commit 8bd178f165f2595bdaf596b4b015e77a2d48814f
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 13:49:07 2022 -0300

    Improve list1 readability

commit 740bdbdc4720401fc0e14ba6a2343fc7f04878b2
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 13:43:45 2022 -0300

    Remove unused file

commit 74a51d37bbd361ef7381259a2cf7168b8a92a60b
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 13:43:14 2022 -0300

    Fix regularization, scaling and finish q2

commit 35a11bf8a67542a1b1e3d8e1fdecab00e4dee208
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 04:59:49 2022 -0300

    Splig q1 and q2 notebooks and finish reg

commit db56f78700265cad3ba05b93c7e8194acdcbf658
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 02:32:59 2022 -0300

    Support regularization

commit 0d461c27f95bb1e0a3c0865cfcf81c23b1b6d3dd
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 02:10:29 2022 -0300

    Improve minmaxscaler tests

commit a0d946506b6e60e03d32171ecef7cbeb68746591
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 00:34:11 2022 -0300

    Remove unused import

commit fe051f0b8b30962de7f5a8da08bd382b3014fb3f
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 00:33:14 2022 -0300

    Remove empty lines

commit d939b58fc569b47ba622372e5a8f427080705348
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 00:30:39 2022 -0300

    Update requirements.txt

commit ee129d8fa37a92d194eaf5c444141a0afa548dc6
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sun Apr 24 00:29:22 2022 -0300

    Dataset utils

commit af222969df7238a85a089c2f6ee1d61c07818121
Author: Jefferson Cavalcante <recife.rodrigo@gmail.com>
Date:   Sat Apr 23 23:00:23 2022 -0300

    Improve scaler tests

commit ba1875e09b9e2cdd258cbc40337c96ed

ml/algorithms/optimization.py
@@ -16,8 +16,9 @@ class AbstractSolver:
 
 
 class OrdinaryLeastSquares(AbstractSolver):
-    def __init__(self):
+    def __init__(self, regularization=0.0):
         super().__init__()
+        self.regularization = regularization
 
     def solve(self, model, inputs, outputs):
         """ 
@@ -29,8 +30,9 @@ class OrdinaryLeastSquares(AbstractSolver):
             outputs (numpy.Array): array of outputs for each input
         """
 
-        w = (np.linalg.inv(inputs.T @ inputs) @ inputs.T) @ outputs
-        model.set_weights(w.T)
+        n = inputs.shape[1]
+        W = ((np.linalg.pinv(inputs.T @ inputs + (self.regularization * np.identity(n)))) @ inputs.T) @ outputs
+        model.set_weights(W.T)
 
 class AbstractGradientDescent(AbstractSolver):
     def __init__(self, epochs, learning_rate, regularization) -> None:
@@ -67,9 +69,10 @@ class GradientDescent(AbstractGradientDescent):
             predictions = model.predict(inputs)
             error = outputs - predictions
 
-            # support for the gradient of more activation
-            # functions is planned for future releases
-            gradients = (inputs * error).mean(axis=0, keepdims=True)
+            regularization_term = self.regularization * weights
+            regularization_term[0,0] = 0.0
+            
+            gradients = (inputs * error).mean(axis=0, keepdims=True) - regularization_term
             weights += self.learning_rate * gradients
 
             epoch_error = rmse(outputs, predictions)
@@ -108,7 +111,10 @@ class StochasticGradientDescent(AbstractGradientDescent):
                 prediction = model.predict(inputs[i])
                 error = outputs[i] - prediction
 
-                gradients = (inputs[i] * error)
+                regularization_term = self.regularization * weights
+                regularization_term[0,0] = 0.0
+
+                gradients = (inputs[i] * error) - regularization_term
                 weights += (self.learning_rate * gradients)
 
             predictions = model.predict(inputs)