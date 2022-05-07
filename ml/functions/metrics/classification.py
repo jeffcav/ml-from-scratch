2e1efa2cc84679936f778eb88334c522d37c48b0
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2022-05-07 15:31:30 -0300

    Implement f1-score

--- ml/functions/metrics/classification.py
+++ ml/functions/metrics/classification.py
@@ -5,3 +5,15 @@ from .abstract import AbstractMeasure
 class BinaryAccuracy(AbstractMeasure):
     def measure(self, a, b):
         return np.mean(np.round(a) == np.round(b))
+
+class F1Score(AbstractMeasure):
+    def measure(self, y_truth, y_estimated):
+        true_positives = np.sum(np.logical_and((y_estimated == 1), (y_truth == 1)))
+        false_positives = np.sum(np.logical_and((y_estimated == 1), (y_truth == 0)))
+        false_negatives = np.sum(np.logical_and((y_estimated == 0), (y_truth == 1)))
+
+        precision = true_positives / (true_positives + false_positives)
+        recall = true_positives / (true_positives + false_negatives)
+
+        f1 = 2 * ((precision * recall) / (precision + recall))
+        return f1
