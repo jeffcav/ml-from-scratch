37ecda6cc1c8984747dbaa1814df8827a25acd0e
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2022-06-02 11:23:57 -0300

    Fix layers creation

--- ml/models/nn.py
+++ ml/models/nn.py
@@ -1,21 +1,29 @@
 import numpy as np
 from .base import AbstractModel
 
+NUM_NEURONS=0
+ACTIV_FUNC=1
+
 class MLPRegressor(AbstractModel):
-    def __init__(self, layers=()):
+    def __init__(self, layers, input_width):
         super(MLPRegressor, self).__init__(None)
         self.layers = layers
-        self.params = self.create_layers(layers)
+        self.params = self.create_layers(self.layers, input_width)
 
-        for l in self.params:
-            print(l.shape)
-
-
-    def create_layers(self, layers):
+    # weights are initialized to 0.5
+    def create_layers(self, layers, input_width):
         W = []
-        for i in range(len(layers) - 1):
-            shape = (layers[i][0], layers[i+1][0])
-            W.append(np.zeros((shape)))
+
+        prev_layer_width = input_width
+        #shape = (input_width, self.layers[0][NUM_NEURONS])
+
+        for i in range(len(layers)):
+            curr_layer_width = layers[i][NUM_NEURONS]
+            shape = (prev_layer_width, curr_layer_width)
+            W.append(np.full((shape), 0.5))
+            prev_layer_width = curr_layer_width
+
+            #shape = (layers[i-1][NUM_NEURONS], layers[i][NUM_NEURONS])
 
         return W
 
@@ -25,10 +33,12 @@ class MLPRegressor(AbstractModel):
     def predict(self, x):
         z = x
 
-        for layer_idx in range(len(self.layers) - 1):
+        for layer_idx in range(len(self.layers)):
             W = self.params[layer_idx]
-            f = self.layers[layer_idx][1]
+            f = self.layers[layer_idx][ACTIV_FUNC]
 
-            z = f(W.T @ z)
+            print(W.shape, z.shape)
+
+            z = f(z @ W)
 
         return z
