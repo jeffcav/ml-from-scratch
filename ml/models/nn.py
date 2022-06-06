cf20f0e1710b8635fa999116fdae2f9c207a5748
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2022-06-06 02:25:13 -0300

    Bias only in hidden layers

--- ml/models/nn.py
+++ ml/models/nn.py
@@ -1,30 +1,32 @@
 import numpy as np
 from .base import AbstractModel
 
-NUM_NEURONS=0
-ACTIV_FUNC=1
+NUM_NEURONS_IDX=0
+ACTIV_FUNC_IDX=1
 
 class MLPRegressor(AbstractModel):
     def __init__(self, layers, input_width):
         super(MLPRegressor, self).__init__(None)
         self.layers = layers
-        self.create_layers(self.layers, input_width)
+        self.params = self.create_layers(self.layers, input_width)
 
     # weights are initialized to 0.5
     def create_layers(self, layers, input_width):
-        self.params = []
+        W = []
 
         prev_layer_width = input_width
         for i in range(len(layers)):
-            curr_layer_width = layers[i][NUM_NEURONS] + 1
+            # add bias neuron if it's not the output layer
+            curr_layer_width = layers[i][NUM_NEURONS_IDX] + (1 if i < (len(layers)-1) else 0)
 
             layer_weights = np.random.rand(prev_layer_width, curr_layer_width)
-            self.params.append(layer_weights)
+            W.append(layer_weights)
             
             prev_layer_width = curr_layer_width
+        return W
 
     def fit(self, inputs, outputs):
-        pass
+        self.trained = True
 
     def predict(self, x):
         if not self.trained:
@@ -34,7 +36,7 @@ class MLPRegressor(AbstractModel):
 
         for layer_idx in range(len(self.layers)):
             W = self.params[layer_idx]
-            f = self.layers[layer_idx][ACTIV_FUNC]
+            f = self.layers[layer_idx][ACTIV_FUNC_IDX]
 
             print("z shape:", z.shape, "w shape:", W.shape, "out shape:", z.shape[0], W.shape[1])
 
