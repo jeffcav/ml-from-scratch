Fix backprop for last layer

ml/algorithms/optimization.py
@@ -68,6 +68,15 @@ class GradientDescent(AbstractGradientDescent):
             regularization_term[0,0] = 0.0
 
             gradients = (inputs * error).mean(axis=0, keepdims=True) - regularization_term
+            print("[SHAPES] X", inputs.shape,
+            "params:", model.params.shape,
+            "grads:", gradients.shape,
+            "error", error.shape,
+            "X*error:", (inputs*error).shape,
+            "reg", regularization_term.shape,
+            "xerror mean:", (inputs*error).mean(axis=0, keepdims=True).shape,
+            "lr*grads:", (self.learning_rate * gradients).shape)
+
             model.params += self.learning_rate * gradients
 
             # keep track of training quality
@@ -121,7 +130,7 @@ class StochasticGradientDescent(AbstractGradientDescent):
 
 class BackpropGD(AbstractGradientDescent):
     def __init__(self, epochs, learning_rate, regularization, batch_size=1, metrics=RMSE()) -> None:
-        super(GradientDescent, self).__init__(epochs, learning_rate, regularization, metrics)
+        super(BackpropGD, self).__init__(epochs, learning_rate, regularization, metrics)
         self.batch_size = batch_size
 
     def solve(self, model, inputs, outputs):
@@ -140,11 +149,8 @@ class BackpropGD(AbstractGradientDescent):
 
         training_measurements = []
         for _ in range(self.epochs):
-            # shuffle data
+            # shuffle and batch input data
             shuffle = np.random.permutation(inputs.shape[0])
-            # inputs = inputs[shuffle]
-            # outputs = outputs[shuffle]
-
             num_batches = inputs.shape[0]/self.batch_size
             batches = np.array_split(shuffle, num_batches)
 
@@ -154,17 +160,45 @@ class BackpropGD(AbstractGradientDescent):
 
                 prediction = model.predict(X)
                 error = Y - prediction
-
+                
                 # output layer
-                regularization_term = self.regularization * model.params[layer_idx]
+                output_layer_idx = len(model.params) - 1
+                regularization_term = self.regularization * model.params[output_layer_idx]
                 regularization_term[0,0] = 0.0
 
-                gradients = (X * error).mean(axis=0, keepdims=True) - regularization_term
-                model.params += (self.learning_rate * gradients)
+                gradients = (X * error).mean(axis=0, keepdims=True) - regularization_term.T
 
-                # hidden layers
-                for layer_idx in range(len(model.params) - 2, -1, -1):
-                    pass
+                # print("[SHAPES] X:", X.shape, 
+                #     "params:", model.params[output_layer_idx].shape,
+                #     "gradients:", gradients.shape,
+                #     "error:", error.shape,
+                #     "X*error:", (X*error).shape,
+                #     "reg", regularization_term.T.shape,
+                #     "xerror mean:", (X*error).mean(axis=0, keepdims=True).shape,
+                #     "lr*grads:", (self.learning_rate * gradients).shape)
+
+                model.params[output_layer_idx] += ((self.learning_rate * gradients).T)
+                
+                # grads.append(gradients)
+                # for hidden_layer_idx in range(output_layer_idx - 1, -1, -1):
+                #     outputs_from_next_layer = model.outputs[hidden_layer_idx+1]
+                    
+                #     # needed?
+                #     if hidden_layer_idx == (output_layer_idx - 1):
+                #         delta = error * model.params[output_layer_idx]
+                #     else:
+                #         pass
+
+                #     # output layer
+                #     regularization_term = self.regularization * model.params[hidden_layer_idx]
+                #     regularization_term[0,0] = 0.0
+
+                #     u = model.linear_outputs[hidden_layer_idx]
+                #     grad_u = model.layers[hidden_layer_idx][1].grad(u)
+
+                #     gradient = grad_u * 
+                #     #model.inputs[hidden_layer_idx] (X * error).mean(axis=0, keepdims=True) - regularization_term
+                #     model.params[hidden_layer_idx] += (self.learning_rate * gradients)
 
             # keep track of training quality
             predictions = model.predict(inputs)

ml/models/nn.py
@@ -5,46 +5,59 @@ NUM_NEURONS_IDX=0
 ACTIV_FUNC_IDX=1
 
 class MLPRegressor(AbstractModel):
-    def __init__(self, layers, input_width):
+    def __init__(self, layers, input_width, solver):
         super(MLPRegressor, self).__init__(None)
+        self.solver = solver
         self.layers = layers
         self.params = self.create_layers(self.layers, input_width)
 
-    # weights are initialized to 0.5
     def create_layers(self, layers, input_width):
         W = []
 
         prev_layer_width = input_width
         for i in range(len(layers)):
-            # add bias neuron if it's not the output layer
-            curr_layer_width = layers[i][NUM_NEURONS_IDX] + (1 if i < (len(layers)-1) else 0)
-
-            layer_weights = np.random.rand(prev_layer_width, curr_layer_width)
-            W.append(layer_weights)
+            layer_width = layers[i][NUM_NEURONS_IDX]
+            layer_weights = np.random.rand(prev_layer_width + 1, layer_width) # +1 for bias
+            prev_layer_width = layer_width
             
-            prev_layer_width = curr_layer_width
+            W.append(layer_weights)
         return W
 
     def fit(self, inputs, outputs):
+        inputs = np.c_[np.ones(inputs.shape[0]), inputs]
+        errors = self.solver.solve(self, inputs, outputs)
+
         self.trained = True
+        return errors
 
     def predict(self, x):
         if not self.trained:
-            self.outputs = []
+            self.inputs = []
+            self.linear_outputs = []
+            self.nonlinear_outputs = []
 
         z = x
 
         for layer_idx in range(len(self.layers)):
+            # append bias = 1 to all layers when trained
+            # or to all but the input layer when training
+            if self.trained or (layer_idx > 0 and not self.trained):
+                z = np.c_[np.ones(z.shape[0]), z]
+
             W = self.params[layer_idx]
             f = self.layers[layer_idx][ACTIV_FUNC_IDX]
 
-            print("z shape:", z.shape, "w shape:", W.shape, "out shape:", z.shape[0], W.shape[1])
-
-            z = f(z @ W)
-
-            # keep track of each layer's output during
-            # training, so we can backpropagate error
+            # keep track of each layer's outputs (raw and non-linear)
+            # during training, so we can more easily backpropagate error
             if not self.trained:
-                self.outputs.append(z)
+                self.inputs.append(z)
+
+            u = z @ W
+            if not self.trained:
+                self.linear_outputs.append(u)
+
+            z = f(u)
+            if not self.trained:
+                self.nonlinear_outputs.append(z)
 
         return z